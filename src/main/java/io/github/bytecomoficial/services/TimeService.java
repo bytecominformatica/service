ef3617279fc46d306ea458369a071ebd4c94a2ea
clairtonluz <clairton.c.l@gmail.com>
2014-12-17 19:07:45 -0300

    teste

---
+++ src/main/java/io/github/bytecomoficial/services/TimeService.java
@@ -0,0 +1,20 @@
+package io.github.bytecomoficial.services;
+
+import io.github.bytecomoficial.models.Time;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/time")
+@Produces(MediaType.APPLICATION_JSON)
+public class TimeService {
+
+    @GET
+    public Time get() {
+        return new Time();
+    }
+
+}
+
