e9a22c63a5c105121ad77ad28bcf199860a6ee0e
clairton <clairton@cialne.com.br>
2014-12-24 12:43:53 -0300

    teste

--- src/main/java/io/github/bytecomoficial/services/SecuryteService.java
+++ src/main/java/io/github/bytecomoficial/services/SecuryteService.java
@@ -7,6 +7,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -20,7 +21,7 @@ public class SecuryteService {
 
 	@POST
 	@Path("/login")
-	public DBObject get(@QueryParam("usuario") String usuario,
+	public Response get(@QueryParam("usuario") String usuario,
 			@QueryParam("senha") String senha) throws UnknownHostException {
 		System.out.printf("Usuario: %s%nSenha: %s%n", usuario, senha);
 //		MongoClient client = new MongoClient("localhost", 27017);
@@ -29,7 +30,11 @@ public class SecuryteService {
 		DBCollection collection = database.getCollection("usuario");
 		DBObject document = collection.findOne();
 		System.out.println(document);
-		return document;
+		return Response.ok()
+		    .entity(document.toString())
+		    .header("Access-Control-Allow-Origin","*")
+            .header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+            .build();
 	}
 
 }
