0dc5c5d94c210bc0c539b6305dc326e14ffbc4aa
clairtonluz <clairton.c.l@gmail.com>
2014-12-25 16:22:27 -0300

    adicionando validação

--- src/main/java/io/github/bytecomoficial/services/SecuryteService.java
+++ src/main/java/io/github/bytecomoficial/services/SecuryteService.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -23,18 +24,26 @@ public class SecuryteService {
 	@Path("/login")
 	public Response get(@QueryParam("usuario") String usuario,
 			@QueryParam("senha") String senha) throws UnknownHostException {
-		System.out.printf("Usuario: %s%nSenha: %s%n", usuario, senha);
-//		MongoClient client = new MongoClient("localhost", 27017);
-		MongoClient client = new MongoClient(new MongoClientURI("mongodb://bytecom:bytecom@ds063240.mongolab.com:63240/bytecom"));
+		// MongoClient client = new MongoClient("localhost", 27017);
+		MongoClient client = new MongoClient(
+				new MongoClientURI(
+						"mongodb://bytecom:bytecom@ds063240.mongolab.com:63240/bytecom"));
 		DB database = client.getDB("bytecom");
 		DBCollection collection = database.getCollection("usuario");
 		DBObject document = collection.findOne();
-		System.out.println(document);
-		return Response.ok()
-		    .entity(document.toString())
-		    .header("Access-Control-Allow-Origin","*")
-            .header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
-            .build();
+		if (document == null) {
+			return Response
+					.status(Status.NOT_FOUND)
+					.header("Access-Control-Allow-Origin", "*")
+					.header("Access-Control-Allow-Methods",
+							"GET, POST, DELETE, PUT").build();
+		}
+		return Response
+				.ok()
+				.entity(document.toString())
+				.header("Access-Control-Allow-Origin", "*")
+				.header("Access-Control-Allow-Methods",
+						"GET, POST, DELETE, PUT").build();
 	}
 
 }
