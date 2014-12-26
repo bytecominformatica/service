c4a2fe157b00661df3e85cfdd8085d56dcba9259
clairtonluz <clairton.c.l@gmail.com>
2014-12-26 18:54:34 -0300

    tela de login

--- src/main/java/io/github/bytecomoficial/services/SecuryteService.java
+++ src/main/java/io/github/bytecomoficial/services/SecuryteService.java
@@ -10,6 +10,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
@@ -20,30 +21,38 @@ import com.mongodb.MongoClientURI;
 @Produces(MediaType.APPLICATION_JSON)
 public class SecuryteService {
 
+	private static final String URL = "mongodb://bytecom:bytecom@ds063240.mongolab.com:63240/bytecom";
+
 	@POST
 	@Path("/login")
-	public Response get(@QueryParam("usuario") String usuario,
-			@QueryParam("senha") String senha) throws UnknownHostException {
-		// MongoClient client = new MongoClient("localhost", 27017);
-		MongoClient client = new MongoClient(
-				new MongoClientURI(
-						"mongodb://bytecom:bytecom@ds063240.mongolab.com:63240/bytecom"));
-		DB database = client.getDB("bytecom");
-		DBCollection collection = database.getCollection("usuario");
-		DBObject document = collection.findOne();
-		if (document == null) {
+	public Response get(@QueryParam("login") String login,
+			@QueryParam("password") String passwordHash)
+			throws UnknownHostException {
+		MongoClient client = null;
+		try {
+			MongoClientURI uri = new MongoClientURI(URL);
+			client = new MongoClient(uri);
+			DB database = client.getDB("bytecom");
+			DBCollection collection = database.getCollection("usuario");
+			DBObject document = collection.findOne(new BasicDBObject("login",
+					login).append("password", passwordHash));
+			if (document == null) {
+				return Response
+						.status(Status.NO_CONTENT)
+						.header("Access-Control-Allow-Origin", "*")
+						.header("Access-Control-Allow-Methods",
+								"GET, POST, DELETE, PUT").build();
+			}
 			return Response
-					.status(Status.NOT_FOUND)
+					.ok()
+					.entity(document.toString())
 					.header("Access-Control-Allow-Origin", "*")
 					.header("Access-Control-Allow-Methods",
 							"GET, POST, DELETE, PUT").build();
+		} finally {
+			if (client != null) {
+				client.close();
+			}
 		}
-		return Response
-				.ok()
-				.entity(document.toString())
-				.header("Access-Control-Allow-Origin", "*")
-				.header("Access-Control-Allow-Methods",
-						"GET, POST, DELETE, PUT").build();
 	}
-
 }
